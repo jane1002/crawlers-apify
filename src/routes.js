7abe841670395efd2ce62f4693db256986e671ee
jane1002 <jane.fan1002@gmail.coom>
2020-06-12 08:50:08 -0500

    Add docket num to each filling.

--- src/routes.js
+++ src/routes.js
@@ -22,10 +22,12 @@ exports.handleDockets = async ($, requestQueue) => {
             const docketNum = $(el).find('td > strong > a').eq(0).text().trim();
             let docketLink = $(el).find('td > strong > a').attr('href');
 
-            docketLink = new URL(docketLink, baseUrl);
-            await requestQueue.addRequest({
-                url: docketLink.href
-            });
+            if(docketLink) {
+                docketLink = new URL(docketLink, baseUrl);
+                await requestQueue.addRequest({
+                    url: docketLink.href
+                });
+            }
 
             const filings = $(el).find('td').eq(1).text().trim();
             const utility = $(el).find('td').eq(2).text().trim();
@@ -52,36 +54,45 @@ exports.handleDockets = async ($, requestQueue) => {
         });
     }
 };
-
+/*
+ some filings don't have download link, 15599.
+ */
 exports.handleFilings = async ($, requestQueue) => {
     // Handle details
     log.info('[handle filings]');
     const lastIndex = $('table tr').get().length;
-    log.info(lastIndex);
-    const docketNum = $('.layoutHeader h1').text();
+    // log.info(lastIndex);
+    let docketNum = $('.layoutHeader h1').text();
     console.log('docketNum: ', docketNum);
+    const pattern = /[0-9]*$/;
+    docketNum = pattern.exec(docketNum)[0];
 
     $('table').find('tr').each(async (index, el) => {
         let fillings = {};
         if(index != 0 && index < lastIndex - 1) {
-            const docketNum = $(el).find('td > strong > a').eq(0).text().trim();
-            let docketLink = $(el).find('td > strong > a').attr('href');
+            let itemNum;
+            let itemLink = $(el).find('td > strong > a').attr('href');
 
-            docketLink = new URL(docketLink, baseUrl);
-            await requestQueue.addRequest({
-                url: docketLink.href
-            });
+            if(itemLink) {
+                itemNum = $(el).find('td > strong > a').eq(0).text().trim();
+                itemLink = new URL(itemLink, baseUrl);
+                await requestQueue.addRequest({
+                    url: itemLink.href
+                });
+            } else {
+                itemNum = $(el).find('td').eq(0).text().trim();
+            }
 
             const fileStamp = $(el).find('td').eq(1).text().trim();
             const party = $(el).find('td').eq(2).text().trim();
             const description = $(el).find('td').eq(3).text().trim();
-
-            fillings.itemNum = docketNum;
+            fillings.docketNum = docketNum;
+            fillings.itemNum = itemNum;
             fillings.fileStamp = fileStamp;
             fillings.party = party;
             fillings.description = description;
             // console.log(docketNum, filings, utility, description);
-            console.log(JSON.stringify(fillings));
+            // console.log(JSON.stringify(fillings));
             exportJsonObjToCSV(fillings, 'filings.csv');
         }
     });
